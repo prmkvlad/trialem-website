4173ae9ead0e07fad6dafa1bba1f7b02cd91c459
Vlad Pryimak <prmk.vlad@gmail.com>
2024-07-26 00:22:48 +0300

    Improve header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
+/* fixed header */
+const headerEl = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+	if (window.scrollY > headerEl.offsetHeight) {
+		headerEl.classList.add('scrolled');
+	} else {
+		headerEl.classList.remove('scrolled');
+	}
+})
+
 const swiper = new Swiper('.feedback .swiper', {
 	slidesPerView: 2,
 
