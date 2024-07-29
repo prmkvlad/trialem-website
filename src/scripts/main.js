2ef4276f3be8a8d1d0478fb834af8a90286a4006
Vlad Pryimak <prmk.vlad@gmail.com>
2024-07-29 20:04:46 +0300

    Add bicycles page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,10 +11,35 @@ window.addEventListener('scroll', () => {
 
 const swiper = new Swiper('.feedback .swiper', {
 	slidesPerView: 2,
-
 	navigation: {
 		nextEl: '.slider-btn--next',
 		prevEl: '.slider-btn--prev',
 	}
-
 });
+
+/* tabs */
+
+const showTabs = (tabsNav, tabsContent) => {
+	tabsNav?.forEach((navLink, index) => {
+		navLink?.addEventListener('click', () => {
+			tabsNav.forEach((link) => {
+				link.classList.remove('active');
+			})
+			navLink.classList.add('active');
+
+			let navIndex = index;
+
+			tabsContent.forEach((contentItem, index) => {
+				contentItem.classList.remove('active');
+
+				if (index === navIndex) {
+					contentItem.classList.add('active');
+				}
+			});
+		});
+	});
+};
+
+const tabsNav = document.querySelectorAll('.bikes .tabs-nav__link');
+const tabsContent = document.querySelectorAll('.bikes .tabs-content');
+showTabs(tabsNav, tabsContent);
