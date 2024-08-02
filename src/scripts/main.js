b103ef3f19405c677c6d0bc82eac30c693751618
Vlad Pryimak <prmk.vlad@gmail.com>
2024-08-02 18:12:05 +0300

    Add adaptability for feedback section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,10 +10,15 @@ window.addEventListener('scroll', () => {
 })
 
 const swiper = new Swiper('.feedback .swiper', {
-	slidesPerView: 2,
+	slidesPerView: 1,
 	navigation: {
 		nextEl: '.slider-btn--next',
 		prevEl: '.slider-btn--prev',
+	},
+	breakpoints: {
+		992: {
+			slidesPerView: 2
+		}
 	}
 });
 
